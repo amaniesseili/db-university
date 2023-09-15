75b5690901d7627b30ad90726933de7fc7796e14
amani esseili <esseiliamani@hotmail.com>
2023-09-15 10:26:09 +0200

    MOD..

--- query.sql
+++ query.sql
@@ -1,9 +1,9 @@
 
 
--- 1. Selezionare tutti gli studenti nati nel 1990 (160) ( sbagliata)
+-- 1. Selezionare tutti gli studenti nati nel 1990 (160) 
 SELECT *
 FROM `students` 
-WHERE `date_of_birth` = 1990-01-01; 
+WHERE (`date_of_birth`) = 1990 ; 
 
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479) 
@@ -15,23 +15,25 @@ WHERE `cfu` > 10;
 --3. Selezionare tutti gli studenti che hanno più di 30 anni (sbagliata)
 SELECT *
 FROM `students`
-YEAR (CURRENT_DATE()) - 
-WHERE (date_of_birth) > 30
+WHERE YEAR (CURRENT_DATE) - 
+YEAR(`date_of_birth`) > 30;
+
+
 
 
 --4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
 --laurea (286)
 SELECT * 
 FROM `courses`
-WHERE period = 1 semester 
-AND year  = 1;
+WHERE `period` = "1 semester "
+AND `year`  = 1;
 
 --5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
 --20/06/2020 (21)
 SELECT *
 FROM `exams`
-WHERE date = `2020-06-20` 
-AND hour > `14:00:00` ;
+WHERE `date` = "2020-06-20" 
+AND `hour` > "14:00:00" ;
 
 --6. Selezionare tutti i corsi di laurea magistrale (38)
 
